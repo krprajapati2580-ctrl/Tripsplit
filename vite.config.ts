f3fa68b1181c7ba61dc2863c14b41978bedab7a4
krprajapati2580-ctrl <krprajapati.2580@gmail.com>
2026-07-01 02:01:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-refresh' // or whatever your framework plugin is
 
 export default defineConfig({
-  base: './', 
+  base: '/Demo_CleanEmptyCompose/', // Add this exact line with your repo name!
   plugins: [react()],
 })
